1a618258b13472263cae4ee8c7758b8348812efb
nsnull0 <info@yoseph.ws>
2024-01-12 02:35:33 +0900

    [modify] add passportnft

--- contracts/PasspostFinance-NFT.sol
+++ contracts/PasspostFinance-NFT.sol
@@ -4,14 +4,26 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract YCBShareYield is ERC721, ERC721Pausable, Ownable {
-    uint256 private _nextTokenId;
+    struct Stake {
+        uint256 amount;
+        uint256 startTime;
+    }
 
-    constructor(address initialOwner)
+    IERC20 public sToken;
+    uint256 private _nextTokenId;
+    string private constant STATIC_URI = "ipfs://bafybeigluhjtgca3gminf75i47n5sjpdqkf2dajloosdcsyqelaopakyxy";
+
+    mapping(uint256 => Stake[]) public stakes;
+
+    constructor(address initialOwner, address _sToken)
         ERC721("YCBShareYield", "YCBSY")
         Ownable(initialOwner)
-    {}
+    {
+        sToken = IERC20(_sToken);
+    }
 
     function pause() public onlyOwner {
         _pause();
@@ -21,18 +33,45 @@ contract YCBShareYield is ERC721, ERC721Pausable, Ownable {
         _unpause();
     }
 
-    function safeMint(address to) public {
+    function stakeTokens(uint256 tokenId, uint256 amount) public whenNotPaused {
+        _requireOwned(tokenId);
+        sToken.transferFrom(msg.sender, address(this), amount);
+        stakes[tokenId].push(Stake(amount, block.timestamp));
+    }
+
+    function unstakeTokens(uint256 tokenId) public {
+        _requireOwned(tokenId);
+        uint256 totalAmount = 0;
+        for (uint i = 0; i < stakes[tokenId].length; i++) {
+            totalAmount += stakes[tokenId][i].amount;
+        }
+
+        delete stakes[tokenId];
+
+        sToken.transfer(msg.sender, totalAmount);
+    }
+
+    function safeMint(address to) public whenNotPaused {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
     }
 
     // The following functions are overrides required by Solidity.
-
     function _update(address to, uint256 tokenId, address auth)
         internal
         override(ERC721, ERC721Pausable)
         returns (address)
     {
         return super._update(to, tokenId, auth);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721)
+        returns (string memory)
+    {
+       _requireOwned(tokenId);
+        return STATIC_URI;
     }
 }
--- test/passport_finance_test.js
+++ test/passport_finance_test.js
@@ -1,18 +1,59 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { parseUnits, parseEther } = require("ethers");
 
-describe("Passport Finance Contract", function() {
+const tokenUri = 'ipfs://bafybeigluhjtgca3gminf75i47n5sjpdqkf2dajloosdcsyqelaopakyxy';
+
+describe("Passport Finance Contract", function () {
     let owner;
     let customer;
     let nftFactory;
+    let mockErc20;
 
-    beforeEach(async function() {
+    beforeEach(async function () {
         const _nft = await ethers.getContractFactory("YCBShareYield");
+        const _mockErc20 = await ethers.getContractFactory("MockERC20");
         [owner, customer] = await ethers.getSigners();
-        nftFactory = await _nft.deploy(owner.address);
+        mockErc20 = await _mockErc20.deploy("MockToken", "MTT");
+        nftFactory = await _nft.deploy(owner.address, mockErc20.getAddress());
+
+        await mockErc20.mint(customer.address, parseUnits("10000000", 18));
     })
 
-    it("everyone can mint", async function() {
-    	expect(await nftFactory.connect(customer).safeMint(customer.address)).to.not.be.reverted;
+    it("everyone can mint", async function () {
+        expect(await nftFactory.connect(customer).safeMint(customer.address)).to.not.be.reverted;
+    });
+
+    it("when paused can't mint", async function () {
+        await nftFactory.pause();
+        await expect(nftFactory.connect(customer).safeMint(customer.address)).to.be.revertedWithCustomError(nftFactory, "EnforcedPause");
+        await nftFactory.unpause();
+        await expect(nftFactory.connect(customer).safeMint(customer.address)).to.not.be.reverted;
+    })
+
+    it("tokenURI should be static", async function () {
+        const mintTx = await nftFactory.connect(customer).safeMint(customer.address);
+
+        const _tokenUri = await nftFactory.tokenURI(0);
+        expect(_tokenUri).to.equal(tokenUri);
+    });
+
+    it("Stake token", async function () {
+        await nftFactory.connect(customer).safeMint(customer.address);
+        await mockErc20.connect(customer).approve(nftFactory.getAddress(), parseEther("1000"));
+        await expect(nftFactory.connect(customer).stakeTokens(0, parseEther("1"))).to.not.be.reverted;
+
+        await nftFactory.pause();
+        await expect(nftFactory.connect(customer).stakeTokens(0, parseEther("1"))).to.be.reverted;
+
+        await nftFactory.unpause();
+        await expect(nftFactory.connect(customer).stakeTokens(0, parseEther("1"))).to.not.be.reverted;
+    });
+
+    it("Unstake token", async function () {
+        await nftFactory.connect(customer).safeMint(customer.address);
+        await mockErc20.connect(customer).approve(nftFactory.getAddress(), parseEther("1000"));
+        await expect(nftFactory.connect(customer).stakeTokens(0, parseEther("1"))).to.not.be.reverted;
+        await nftFactory.pause();
     });
 })
